ee963753d2aa8692b43ee92348d1af14d9af3855
AnaliceMarques <analicemarquessl@gmail.com>
2023-08-09 16:28:01 -0300

    Structure js

--- modulo2-logica-programacao/comparacao.js
+++ modulo2-logica-programacao/comparacao.js
@@ -1,1 +1,37 @@
 console.log("=== COMPARAÇÃO ===");
+
+const comparar = () => {
+  const x = Number(document.getElementById("x").value || 0);
+  const y = Number(document.getElementById("y").value || 0);
+
+  const relacao = document.getElementById("relacao").value;
+
+  let result;
+  switch (relacao) {
+    case "maior":
+      result = x > y;
+      break;
+    case "menor":
+      result = x < y;
+
+      break;
+    case "maiorIgual":
+      result = x >= y;
+
+      break;
+    case "menorIgual":
+      result = x <= y;
+
+      break;
+    case "igual":
+      result = x == y;
+
+      break;
+    case "diferente":
+      result = x != y;
+
+      break;
+  }
+
+  document.getElementById("result").value = result;
+};
