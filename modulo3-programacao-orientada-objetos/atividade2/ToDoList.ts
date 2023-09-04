d5eefc0ae21602d35b96b7d9f4e8fe1566bbce77
AnaliceMarques <analicemarquessl@gmail.com>
2023-09-04 18:13:10 -0300

    Atividade 2

--- modulo3-programacao-orientada-objetos/atividade2/ToDoList.ts
+++ modulo3-programacao-orientada-objetos/atividade2/ToDoList.ts
@@ -1,16 +1,76 @@
 class Tarefa {
   descricao: string;
   concluida: boolean = false;
+  prioridade?: string;
 
-  constructor(descricao: string) {
+  constructor(descricao: string, prioridade?: string) {
     this.descricao = descricao;
+    this.prioridade = prioridade;
   }
 
-  marcarConcluida() {
+  marcarConcluida(): void {
     this.concluida = true;
   }
 
-  toString() {
+  toString(): string {
     return `${this.descricao} ${this.concluida ? "(Concluída)" : "(Pendente)"}`;
   }
 }
+
+const tarefa = new Tarefa("Estudar");
+console.log(tarefa.toString());
+tarefa.marcarConcluida();
+console.log(tarefa.toString());
+
+class ListaDeTarefas {
+  tarefas: Tarefa[] = [];
+
+  adicionarTarefa(descricao: string): void {
+    const novaTarefa = new Tarefa(descricao);
+    this.tarefas.push(novaTarefa);
+  }
+
+  listarTarefas(): void {
+    for (let tarefa of this.tarefas) {
+      console.log(
+        `${tarefa.descricao} ${tarefa.concluida ? "(Concluída)" : "(Pendente)"}`
+      );
+    }
+  }
+}
+
+const listaDeTarefas = new ListaDeTarefas();
+console.log("Lista de tarefas");
+listaDeTarefas.adicionarTarefa("Estudar");
+listaDeTarefas.adicionarTarefa("Descansar");
+listaDeTarefas.listarTarefas();
+listaDeTarefas.tarefas[0].marcarConcluida();
+listaDeTarefas.listarTarefas();
+
+class ListaPrioritaria extends ListaDeTarefas {
+  prioridade: string = "";
+  listaPrioritaria: Tarefa[] = [];
+
+  adicionarTarefaPrioritaria(descricao: string, prioridade: string) {
+    const novaTarefa = new Tarefa(descricao, prioridade);
+    this.listaPrioritaria.push(novaTarefa);
+  }
+
+  listarTarefas(): void {
+    for (let tarefa of this.listaPrioritaria) {
+      console.log(
+        `${tarefa.descricao} - Prioridade: ${tarefa.prioridade} ${
+          tarefa.concluida ? "(Concluída)" : "(Pendente)"
+        }`
+      );
+    }
+  }
+}
+
+const listaDeTarefasPrioritaria = new ListaPrioritaria();
+console.log("Lista de tarefas prioritaria");
+listaDeTarefasPrioritaria.adicionarTarefaPrioritaria("Estudar", "Alta");
+listaDeTarefasPrioritaria.adicionarTarefaPrioritaria("Descansar", "Baixa");
+listaDeTarefasPrioritaria.listarTarefas();
+listaDeTarefasPrioritaria.listaPrioritaria[0].marcarConcluida();
+listaDeTarefasPrioritaria.listarTarefas();
